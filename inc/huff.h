e155cb75b172cbed192857f9dc3fe0ed0839561a
Valdir de Souza Junior <vsj@ic.ufal.br>
2017-04-05 15:51:05 -0300

    Estruturação Projeto básica

---
+++ inc/huff.h
@@ -0,0 +1,18 @@
+//Arquivo huff.h
+        typedef struct Huff Huff;
+        typedef struct Node Node;
+        Huff * NewHuff();
+        Huff * NewNode(char c, int freq);
+        Save the tree using “*” and the pre-order traversal
+         struct Node {
+                char c;
+                int freq;
+                Node * next;
+                Node * left;
+                Node * right;
+        };
+        struct Huff {
+                Node * head;
+                int size;        
+        };    
+
