fd00d9626260e6e8806524518a143f670729c6ee
Valdir de Souza Junior <jr140797@gmail.com>
2017-04-06 22:49:39 -0300

    Pedrobranch (#8)
    * Add biblioteca binary.h
    
    * Agora sim add binary.h...
    
    * Corrigindo encapsulamento e CMakeLists.txt.
    
    * Corrigindo '#ifndef' dos arquivos .h

---
+++ src/binary.c
@@ -0,0 +1,51 @@
+//
+// Created by Pedro on 06/04/2017.
+//
+#include "../inc/binary.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char * IntegerToBinary(int integer, int string_size) {
+
+    char* binary_number = (char*)malloc(sizeof(char)*(string_size+1));
+    int i, rest;
+    int flag = 0;
+    rest = integer%2;
+    integer = integer/2;
+    for(i = string_size-1; i >= 0; --i) {
+        if(integer != 0) {
+            if(rest) {
+                binary_number[i] = '1';
+            } else {
+                binary_number[i] = '0';
+            }
+            rest = integer%2;
+            integer = integer/2;
+        } else if(integer == 0 && !flag) {
+            if(rest) {
+                binary_number[i] = '1';
+            } else {
+                binary_number[i] = '0';
+            }
+            flag = 1;
+        } else {
+            binary_number[i] = '0';
+        }
+    }
+    binary_number[string_size] = '\0';
+    return binary_number;
+}
+
+int BinaryToInteger(char* binary_number) {
+
+    int number = 0;
+    int string_size = strlen(binary_number);
+    int i, j;
+    for(i = string_size-1, j = 0; i >= 0; --i, ++j) {
+        if(binary_number[i] == '1') {
+            number += pow(2,j);
+        }
+    }
+    return number;
+}
