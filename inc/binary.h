dd5742b9e6ae80fb65de501e91962845c0ba06be
pedromaf <pmaf@ic.ufal.br>
2017-04-06 21:08:36 -0300

    Agora sim add binary.h...

--- inc/binary.h
+++ inc/binary.h
@@ -1,8 +1,19 @@
 //
 // Created by Pedro on 06/04/2017.
 //
+#ifndef BINARY_H
+#define BINARY_H
 
-#ifndef UNTITLED_BINARY_H
-#define UNTITLED_BINARY_H
+/*
+ * It receives as parameter an integer, 'integer', and the size, 'string_size', corresponding to the number of bits of the desired binary number;
+ * Returns a string with the binary number.
+ */
+char * IntegerToBinary(int integer, int string_size);
 
-#endif //UNTITLED_BINARY_H
+/*
+ * It receives as parameter an string, 'binary_number', of the binary number.
+ * Returns a integer of the converted binary number.
+ */
+int BinaryToInteger(char* binary_number);
+
+#endif
