390fa15816575418e3a9238c758fe86f517bd27b
pedromaf <pmaf@ic.ufal.br>
2017-04-06 21:31:44 -0300

    Corrigindo '#ifndef' dos arquivos .h

--- inc/huff.h
+++ inc/huff.h
@@ -1,3 +1,6 @@
+#ifndef HUFF_H
+#define HUFF_H
+
 #include <stdio.h>
 #include "tabela.h"
 
@@ -14,41 +17,4 @@ void GeraTabelaConversao(Node*head, Tabela ** tabela, ElementoTabela ** percurso
 void PrintList(Node * node);
 //Save the tree using �*� and the pre-order traversal
 
-/*/
-
-
- typedef struct Huff Huff;
-typedef struct Node Node;
-Huff * NewHuff();
-Huff * MakeTree(unsigned int * frequencias);
-Node * NewNode(char c, int freq);
-Node * PopNode(Huff * huff);
-void AddNode(Huff * huff, Node * newNode);
-void PrintPreOrder(Node * head);
-void PrintList(Node * node);
-
-         struct Tabela {
-          	//Array de elementos
-            ElementoTabela **elems;
-            int size; //256 SEMPRE
-        };
-        struct ElementoTabela {
-
-          	unsigned char c;
-            int size;
-            Linha * linha;
-        };
-        struct Linha{
-          char i;
-          Linha * next;
-    		}
-
-
-        d
-
-        a -> 0 0 1
-        b -> 1 0 1
-        c -> ...
-        d -> ....
-        f -> ....
- */
+#endif
--- inc/tabela.h
+++ inc/tabela.h
@@ -1,13 +1,13 @@
 //
 // Created by Valdir Jr on 06/04/2017.
 //
-
-#ifndef TABELAH
-#define TABELAH
+#ifndef TABELA_H
+#define TABELA_H
 
 typedef struct ElementoTabela ElementoTabela;
 typedef struct Tabela Tabela;
 typedef struct Linha Linha;
+
 Tabela * CreateTabela();
 ElementoTabela * CreateElementoTabela();
 Linha * CreateLinha(char i);
@@ -16,19 +16,4 @@ char Dequeue(ElementoTabela * elem);
 ElementoTabela * CreateCopiaElemento(Linha * head);
 void PrintLinha(Linha * linha);
 
-/*
-Tabela:
-
-AABB  =  00100110 10------
-Freq * Nº Bits + ... +... =  num % 8 = x - 8 =  lixos.
-2 * 3 + 2*2 = 10 % 8 =  2  - 8 =  6 lixos
-Elementos 98  (2) -> 1 -> 0
-Elementos[97] (3) -> 0 -> 0 -> 1
-Elementos[0] -> (Linha)
-Elementos[1]  ->  (Linha)
-Elementos[2] -> (Linha)
-Elementos[3]  -> (linha)
-...
-Elementos[255] ->Linha
-*/
 #endif
