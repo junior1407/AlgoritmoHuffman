64449fb7b8fac03d9164630170dcf0e7826fbd32
Jardel Costa <jfcosta@ic.ufal.br>
2017-05-06 21:49:04 -0300

    Adicionar funções para checar avô, irmão e tio na Redblack

--- RedBlack/inc/redblack.h
+++ RedBlack/inc/redblack.h
@@ -10,4 +10,24 @@ typedef struct Redblack Redblack;
  * Data Structure for the Redblack Tree Root.
  */
 typedef struct Root Root;
+
+/*
+ * Creates a New Redblack Tree Root.
+ */
+Root * CreateRoot();
+
+/*
+ * Return Grandparent's Node.
+ */
+Redblack * Grandparent(Redblack * rb);
+
+/*
+ * Return Brother's Node.
+ */
+Redblack * Brother(Redblack * rb);
+
+/*
+ * Return Uncle's Node.
+ */
+Redblack * Uncle(Redblack * rb);
 #endif
--- RedBlack/src/redblack.c
+++ RedBlack/src/redblack.c
@@ -1,10 +1,11 @@
 #include "../inc/redblack.h"
+#include <stdlib.h>
 
 struct Redblack {
 
     int value;
     char color; // r = Red, b = Black
-    struct Redblack * father;
+    struct Redblack * parent;
     struct Redblack * right;
     struct Redblack * left;
 };
@@ -12,3 +13,25 @@ struct Redblack {
 struct Root {
     Redblack * root;
 };
+
+Root * CreateRoot() {
+    Root * root = malloc(sizeof(Root));
+    root->root = NULL;
+    return root;
+}
+
+Redblack * Grandparent(Redblack * rb) {
+    return rb->parent->parent;
+}
+
+Redblack * Brother(Redblack * rb) {
+    if(rb == rb->parent->left) {
+        return rb->parent->right;
+    } else {
+        return rb->parent->left;
+    }
+}
+
+Redblack * Uncle(Redblack * rb) {
+    return Brother(rb->parent);
+}
